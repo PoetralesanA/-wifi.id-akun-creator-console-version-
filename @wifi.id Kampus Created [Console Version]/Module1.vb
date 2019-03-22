00ddf33ec5ac6ba6f1c634af8d949a14be62f434
PoetralesanA <poetralesana13@gmail.com>
2019-03-22 09:12:08 +0800

    xxx

---
+++ @wifi.id Kampus Created [Console Version]/Module1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        ForegroundColor = ConsoleColor.Green
+        WriteLine("{{{{ Kampus Creator @wifi.id }}}}" & vbNewLine)
+a:
+
+        Try
+            Dim result As String
+            Dim Nim, tahun, tanggal_lahir As Integer
+            ForegroundColor = ConsoleColor.Gray
+            Write("Input Nim => ")
+            Nim = ReadLine()
+            Write("Input Tahun => ")
+            tahun = ReadLine()
+            Write("Input Tanggal Lahir => ")
+            tanggal_lahir = ReadLine()
+
+            result = ("Username : " & Nim & "@ut.ac.id" & vbNewLine _
+                & "Password : " & tahun & tanggal_lahir)
+            Write("========== Result ==========" & vbNewLine _
+                     & result & vbNewLine &
+                      "============================")
+        Catch ex As Exception
+            ForegroundColor = ConsoleColor.Red
+            Write("Failed!, Type Data String Ditolak!")
+        End Try
+        ReadKey()
+        WriteLine()
+        GoTo a
+    End Sub
+
+End Module
